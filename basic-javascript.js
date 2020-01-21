9b152fe7edee03b7c950e52a6e056dbd44ae7ab2
Ernest <ernesmanurungg@gmail.com>
2020-01-21 14:21:10 +0700

    [Ernest] - Exercise 2

--- basic-javascript.js
+++ basic-javascript.js
@@ -22,5 +22,45 @@ while (angka1 <= angka2) {
 }
 
 /* Exercise 2
+Kamu diminta untuk memprogram suatu game sederhana,GlintsWarrior namanya.
+Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), yaitu nama dan peran.
+Variabel peran harus memiliki isi data, bila kosong pemain akan diberikan 
+peringatan berupa "Pilih Peranmu untuk memulai game".
+Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir.
+Tugas kamu adalah untuk membuat program yang mengecek isi variabel peran serta 
+mengeluarkan respon sesuai isi variabel tersebut.
 
+Variabel tetap di-input secara manual.
+Variabel nama dan peran dapat diisi apa saja.
+Nama tidak perlu dicek sama persis seperti contoh input/output
+Lakukan pengecekan setiap peran menggunkan guard clause
 */
+
+var nama = 'maria';
+var peran = 'Penyihir';
+
+if (nama === '' && peran == '') {
+    console.log('Pilih Peranmu untuk memulai game.')
+}
+
+if (nama !== '' && peran == '') {
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+}
+
+if (nama !== '' && peran == 'Ksatria') {
+    console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', Tugas kamu adalah menyerang musuh!');
+}
+
+if (nama !== '' && peran == 'Tabib') {
+    console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ' , kamu akan membantu temanmu yang terluka.');
+}
+
+if (nama !== '' && peran === 'Penyihir') {
+    console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+}
+
+
+

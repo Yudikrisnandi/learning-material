16bc28c232c01df19fe74ee25f8df8ef3879283d
Ernest <ernesmanurungg@gmail.com>
2020-01-21 16:26:33 +0700

    [Ernest] - Exercise 4

--- basic-javascript.js
+++ basic-javascript.js
@@ -5,21 +5,21 @@
     Ketika angkanya habis di bagi 2 dan 5, di depan angkanya di tambah string "Forza Milan".
 */
 
-var angka1 = 1;
-var angka2 = 30;
+// var angka1 = 1;
+// var angka2 = 30;
 
-while (angka1 <= angka2) {
-    if(angka1 % 2 === 0) {
-        console.log('Forza ' + angka1);
-    } else if (angka1 % 5 === 0) {
-        console.log('Milan ' + angka1);
-    } else if (angka1 % 5 === 0 && angka1 % 2 === 0) {
-        console.log('Forza Milan ' + angka1);
-    } else {
-        console.log(angka1)
-    }
-    angka1++;
-}
+// while (angka1 <= angka2) {
+//     if(angka1 % 2 === 0) {
+//         console.log('Forza ' + angka1);
+//     } else if (angka1 % 5 === 0) {
+//         console.log('Milan ' + angka1);
+//     } else if (angka1 % 5 === 0 && angka1 % 2 === 0) {
+//         console.log('Forza Milan ' + angka1);
+//     } else {
+//         console.log(angka1)
+//     }
+//     angka1++;
+// }
 
 /* Exercise 2
 Kamu diminta untuk memprogram suatu game sederhana,GlintsWarrior namanya.
@@ -36,31 +36,31 @@ Nama tidak perlu dicek sama persis seperti contoh input/output
 Lakukan pengecekan setiap peran menggunkan guard clause
 */
 
-var nama = 'maria';
-var peran = 'Penyihir';
+// var nama = 'maria';
+// var peran = 'Penyihir';
 
-if (nama === '' && peran == '') {
-    console.log('Pilih Peranmu untuk memulai game.')
-}
+// if (nama === '' && peran == '') {
+//     console.log('Pilih Peranmu untuk memulai game.')
+// }
 
-if (nama !== '' && peran == '') {
-    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
-}
+// if (nama !== '' && peran == '') {
+//     console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+// }
 
-if (nama !== '' && peran == 'Ksatria') {
-    console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
-    console.log('Halo ' + peran + ' ' + nama + ', Tugas kamu adalah menyerang musuh!');
-}
+// if (nama !== '' && peran == 'Ksatria') {
+//     console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
+//     console.log('Halo ' + peran + ' ' + nama + ', Tugas kamu adalah menyerang musuh!');
+// }
 
-if (nama !== '' && peran == 'Tabib') {
-    console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
-    console.log('Halo ' + peran + ' ' + nama + ' , kamu akan membantu temanmu yang terluka.');
-}
+// if (nama !== '' && peran == 'Tabib') {
+//     console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
+//     console.log('Halo ' + peran + ' ' + nama + ' , kamu akan membantu temanmu yang terluka.');
+// }
 
-if (nama !== '' && peran === 'Penyihir') {
-    console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
-    console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
-}
+// if (nama !== '' && peran === 'Penyihir') {
+//     console.log('Selamat datang di Dunia GlintsWarrior, ' + nama);
+//     console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+// }
 
 
 /* Exercise 3
@@ -78,58 +78,56 @@ console.log(numberLetters('y7d1kr1sn4nd1')); // mas yudi
 console.log(numberLetters('b4d41')); // badai
 */
 
-function numberLetters(kata) {
+// function numberLetters(kata) {
 
-    var kataBaru = []
+//     var kataBaru = []
 
-    for(var i = 0; i <= kata.length; i++) {
+//     for(var i = 0; i <= kata.length; i++) {
 
-        switch (kata[i]) {
+//         switch (kata[i]) {
             
-            case "1":
-                kataBaru[i] = "i";    
-                break;
-            case "4":
-                kataBaru[i] = "a";
-                break;
-            case "3":
-                kataBaru[i] = "e";
-                break;
-            case "7":
-                kataBaru[i] = "u";
-                break;
-            case "0":
-                kataBaru[i] = "o"
-            default:
-                kataBaru[i] = kata[i];
-                break;
-        } 
+//             case "1":
+//                 kataBaru[i] = "i";    
+//                 break;
+//             case "4":
+//                 kataBaru[i] = "a";
+//                 break;
+//             case "3":
+//                 kataBaru[i] = "e";
+//                 break;
+//             case "7":
+//                 kataBaru[i] = "u";
+//                 break;
+//             case "0":
+//                 kataBaru[i] = "o"
+//             default:
+//                 kataBaru[i] = kata[i];
+//                 break;
+//         } 
+//     }
 
-        // }
-        // if (kata[i] === "1") {
-        //     kataBaru[i] = "i";
-        // }
-        // if (kata[i] === "4") {
-        //     kataBaru[i] = "a";
-        // } 
-        // if (kata[i] = "3") {
-        //     kataBaru[i] = "e";
-        // } 
-        // if (kata[i] === "7") {
-        //     kataBaru[i] === "u";
-        // } 
-        // if (kata[i] === "0") {
-        //     kataBaru[i] = "o";
-        // } 
-        // if (kata[i] === kata[i]) {
-        //     kataBaru[i] = kata[i];
-        // }
+//     return kataBaru.join('');
+// }
 
+// console.log(numberLetters('prat1w1n7r4m1n1')); // pratiwinuramini
+// console.log(numberLetters('yud1'));
+
+/* Exercise 4 
+Diberikan sebuah array yang berisi kumpulan angka 
+bilangan bulat positif. 
+Buatlah sebuah fungsi yang menerima parameter array 
+yang akan mengembalikan total penjumlahan dari setiap 
+elemen-elemen array tersebut.
+*/
+
+function total(angka) {
+    var output = 0;
+    for (var i = 0; i < angka.length; i++) {
+        output += angka[i];
     }
-    return kataBaru.join('');
+
+    return output;
 }
 
-console.log(numberLetters('prat1w1n7r4m1n1')); // pratiwinuramini
-console.log(numberLetters('yud1'));
-
+console.log(total([4, 2, 8, 6]));
 
